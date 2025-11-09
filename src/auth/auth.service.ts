70b9226a3c11a894f003691173387771fe06693a
Ayodeji Obasan <dejiobasan02@gmail.com>
2025-11-09 19:14:42 +0100

    feat: basic nest api.

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AuthService {
+  register() {
+    return 'hello';
+  }
+
+  login() {
+    return 'hello';
+  }
+
+  logout() {
+    return 'hello';
+  }
+}
