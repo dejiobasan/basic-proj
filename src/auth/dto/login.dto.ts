ef8f4419526535511502d451d02f1ed14c29ee43
Ayodeji Obasan <dejiobasan02@gmail.com>
2025-11-11 14:33:01 +0100

    feat: swagger doc

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,11 +1,31 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsString, IsNotEmpty } from 'class-validator';
 
 export class LoginDto {
   @IsEmail()
   @IsNotEmpty()
+  @ApiProperty()
   email: string;
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   password: string;
+}
+
+export class LoginResponseDto {
+  @ApiProperty()
+  userId: string;
+  @ApiProperty()
+  email: string;
+  @ApiProperty()
+  firstName: string;
+  @ApiProperty()
+  lastName: string;
+  @ApiProperty()
+  phoneNumber: string;
+  @ApiProperty()
+  accessToken: string;
+  @ApiProperty()
+  refreshToken: string;
 }
